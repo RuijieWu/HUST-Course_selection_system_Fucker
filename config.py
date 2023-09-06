0f2ddfcd05b784c823df3cf1e545af78e7470f74
JeRyWu <1365840492@qq.com>
2023-09-06 18:39:15 +0800

    0.2.2

---
+++ config.py
@@ -0,0 +1,3 @@
+SESSION:str = "JSESSIONID=NshnqlbuHRz-O5F4JLweJPWSVfOhe-APFhqoQLbGAtZG8laFYQEl!-232618489; BIGipServerpool-hub-wsxkxt=2970225162.22811.0000"
+CLASS_NAME:str = "钢笔绘画" #!这是个例子
+CLASS_NUMBER:str= "1437248"
