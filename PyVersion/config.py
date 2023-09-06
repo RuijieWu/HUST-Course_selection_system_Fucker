3ff55ac97c9693db9adea61ba43e1441ec572b22
JeRyWu <1365840492@qq.com>
2023-09-06 18:44:45 +0800

    0.2.2

--- PyVersion/config.py
+++ PyVersion/config.py
@@ -1,8 +1,9 @@
+'''
+config file
+Put your info here
+'''
 SESSION:str = "YOUR_SESSION_HERE" #! 以 JSESSIONID= 开头的字符串
 CLASS_NAME:str = "钢笔绘画" #!这是个例子
-<<<<<<< HEAD:PyVersion/config.py
 CLASS_NUMBER:str= "202311437248001"
 COURSE_NUMBER:str="1437248"
-=======
-CLASS_NUMBER:str= "1437248"
->>>>>>> parent of 4b20475 (0.1.1):config.py
+
